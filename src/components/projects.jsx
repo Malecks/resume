43470e43a1e796dffa45321d99153baaf5661e46
malecks <alex@alexmathers.com>
2023-11-11 11:49:57 -0800

    update projects list

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom"
 
+import ServiceThumbnail from '../projects/service-booking/ServiceBookingCover.png'
+import PaletteThumbnail from '../projects/palette-cam/PaletteCam.png'
+import AnalyticsThumbnail from '../projects/nudge-analytics-dashboard/Analytics01.png'
+import DesignSystemThumbnail from '../projects/nudge-design-system/DesignLibrary08.png'
+
 const Projects = () => {
     return (
         <ul className="projects-list">
-            <ProjectItem name="Service Booking" url="league-service-booking" desc="League"/>
-            <ProjectItem name="Palette Cam" url="palette-cam" desc="iOS App"/>
-            <ProjectItem name="Analytics Dashboard" url="nudge-analytics-dashboard" desc="Nudge"/>
-            <ProjectItem name="Design System" url="nudge-design-system" desc="Nudge"/>
+            <ProjectItem name="Service Booking" url="league-service-booking" desc="League" img={ServiceThumbnail}/>
+            <ProjectItem name="Palette Cam" url="palette-cam" desc="iOS App" img={PaletteThumbnail}/>
+            <ProjectItem name="Analytics Dashboard" url="nudge-analytics-dashboard" desc="Nudge" img={AnalyticsThumbnail}/>
+            <ProjectItem name="Design System" url="nudge-design-system" desc="Nudge" img={DesignSystemThumbnail}/>
         </ul>
     )
 }
 
-const ProjectItem = ({name, url, desc}) => {
+const ProjectItem = ({name, url, desc, img}) => {
     return (
-        <li className="project-item">
-            <Link to={url}>
-                <img src="" />
-                <div>
+        <Link to={url} className="project-item">
+            <li>
+                <img className="project-thumbnail" src={img} />
+                <div className="project-info">
                     <h4>
                         {name}
                     </h4>
@@ -24,8 +29,8 @@ const ProjectItem = ({name, url, desc}) => {
                         {desc}
                     </div>
                 </div>
-            </Link>
-        </li>
+            </li>
+        </Link>
     )
 }
 
