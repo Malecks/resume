80460c04263d4e0f32ad582d40b1497c129358b8
malecks <alex@alexmathers.com>
2023-11-19 21:56:15 -0800

    update contact and styles

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,11 @@ const Header = () => {
         <header className="app-header">
             <div>
                 <h3>Alex Mathers (@malecks)</h3>
-                <h4><a href="mailto:alex@alexmathers.com">alex@alexmathers.com</a> | <a href="https://github.com/malecks">GitHub</a> | <a href="https://linkedin.com/in/malecks">LinkedIn</a></h4>
+                <div className='contact'>
+                    <a href="mailto:alex@alexmathers.com">alex@alexmathers.com</a>
+                    <a href="https://github.com/malecks">GitHub</a>
+                    <a href="https://linkedin.com/in/malecks">LinkedIn</a>
+                </div>
             </div>
             <img src={avatar} alt="Avatar"/>
         </header>
