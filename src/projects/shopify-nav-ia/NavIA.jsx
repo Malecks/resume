a78b3412883f08b83d25693e197cab370afb8035
malecks <alex@alexmathers.com>
2024-09-03 12:36:08 -0700

    Add assets

--- src/projects/shopify-nav-ia/NavIA.jsx
+++ src/projects/shopify-nav-ia/NavIA.jsx
@@ -1,9 +1,10 @@
 import VideoPlayer from '../../components/videoPlayer'
 
-// import img01 from './InboxIllustration.jpg'
-// import img02 from './Inbox02.png'
-// import video01 from './InboxOnboarding01_720.mov'
-// import video03 from './InboxOnboarding03_720.mov'
+import img01 from './NavIA01.png'
+import img02 from './NavIA02.png'
+import img03 from './NavIA03.png'
+import video01 from './NavIAVideo01_720.mov'
+import video02 from './NavIAVideo02_720.mov'
 
 const NavIA = () => {
   return (
@@ -18,7 +19,7 @@ const NavIA = () => {
         <p>
             The Shopify mobile app lacked clear hierarchy and structure, particularly when switching between stores and accounts.
         </p>
-        {/* <img src={img01} alt="Shopify Inbox App" /> */}
+        <img src={img03} alt="Shopify Nav and IA revamp" />
       </section>
 
       <section>
@@ -52,8 +53,12 @@ const NavIA = () => {
               Alignment with web admin
             </li>
         </ul>
-        {/* <img src={img02} alt="Previous app state" /> */}
-        <caption>The previous multiple paths for installation, and the poor overview experience</caption>
+        <img src={img01} alt="Previous app state" />
+        <caption>The previous state of the app. Account and store settings were jumbled, with unintuitive paths to switch between them or access settings.</caption>
+
+        <img src={img02} alt="Previous app state" />
+        <caption>Visualizing how we wanted to group like content together, and reduce the complexity of 'adding or removing' stores.</caption>
+
       </section>
 
       <section>
@@ -83,8 +88,8 @@ const NavIA = () => {
         </ul>
 
         <div className='video-wrapper'>
-          <VideoPlayer url={""} />
-          <VideoPlayer url={""} />
+          <VideoPlayer url={video01} />
+          <VideoPlayer url={video02} />
         </div>
         <caption>Prototypes of the new Inbox Onboarding experience, made in Origami Stuido</caption>
       </section>
