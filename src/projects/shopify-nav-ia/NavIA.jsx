b11e62b9c087c217808f3bfb7ffc4931b5059946
malecks <alex@alexmathers.com>
2024-09-04 14:03:28 -0700

    Cleanup nav and IA project

--- src/projects/shopify-nav-ia/NavIA.jsx
+++ src/projects/shopify-nav-ia/NavIA.jsx
@@ -17,43 +17,36 @@ const NavIA = () => {
       <section>
         <h3>TLDR;</h3>
         <p>
-          This project sought to re-define the navigation and IA across the whole of the Shopify mobile app. With considered updates, we hoped to remove unintuitive behaviours, introduce functionality for multi-tasking, ease account and store switching, and create a polished and extensible foundation for future feature work.
+          This project sought to re-define the navigation and IA across the whole of the Shopify mobile app. With considered updates, we removed unintuitive behaviours, introduced functionality for multi-tasking, easy account and store switching, and created a polished and extensible foundation for future feature work.
         </p>
+        <img src={img03} alt="Shopify Nav and IA revamp" />
+        <div className='image-caption'>The updated navigation and IA.</div>
       </section>
 
       <section>
         <h3>My Role: UX Lead</h3>
         <ul>
           <li>Feature design in Figma, interaction design and prototyping in Origami</li>
-          <li>Work cross-functionally with the engineering and product managers as a trifecta member. Presenting and generating alignment with key stakeholders and senior leadership.</li>
+          <li>Working cross-functionally with the engineering and product managers as a trifecta member. Presenting and generating alignment with key stakeholders and senior leadership.</li>
           <li>Pairing closely with developers on implementation details. Documenting and illustrating key decisions.</li>
           <li>Connecting with teams across Shopify to provide context and direction on interconnected projects.</li>
         </ul>
-        <img src={img03} alt="Shopify Nav and IA revamp" />
-        <div className='image-caption'>The updated navigation and IA.</div>
       </section>
-
-      
 
       <section>
         <h3>Problems identified</h3>
         <p>
           The Shopify mobile app lacked clear hierarchy and structure, particularly when switching between stores and accounts. Settings were scattered across different tabs, and accounts and stores were intertwined unintuitively. Many important workflows were hidden behind a bloated 'Store' tab which has no equivalent on the web app. And finally, there was an awkward behaviour to enable and disable your stores on the app.
         </p>
-        <ul>
-            <li>
-              App lacks clear hierarchy and structure
-            </li>
-            <li>
-              The 'Store' tab is bloated and inconsistent
-            </li>
-            <li>
-              Does not align with the navigational structure on web
-            </li>
-            <li>
-              Enabling and disabling your stores is confusing
-            </li>
-        </ul>
+        <img src={img01} alt="Previous app state" />
+        <div className='image-caption'>
+          The previous state of the app. Account and store settings were jumbled, with unintuitive actions to switch between them or access settings.
+        </div>
+        <img src={img02} alt="Previous app state" />
+        <div className='image-caption'>Visualizing how we wanted to group content, and reduce the complexity of 'adding or removing' stores.</div>
+      </section>
+
+      <section>
         <h3>Goals</h3>
         <ul>
             <li>
@@ -69,29 +62,23 @@ const NavIA = () => {
       </section>
 
       <section>
-      <img src={img01} alt="Previous app state" />
-        <div className='image-caption'>
-          The previous state of the app. Account and store settings were jumbled, with unintuitive actions to switch between them or access settings.
-        </div>
-      <img src={img02} alt="Previous app state" />
-      <div className='image-caption'>Visualizing how we wanted to group content, and reduce the complexity of 'adding or removing' stores.</div>
-      </section>
-      <h3>Solution</h3>
-      <p>
-        Our solution involved creating a new idiom in the app of surfaces. The main surface would include all of the features of your store that users were accustomed to: their home tab, analytics, products etc. Accounts and stores were moved to a new side-drawer, creating an elevated surface for contextual information. We also removed the 'Store' tab, and replaced it with a sheet where you can find additional features neatly organized. We styled this bottom sheet to act as an extension of the tab bar, and engineered it to allow for easy multitasking by maintining a unique back-stack for each tab.
-      </p>
-      <ul>
-        <li>Completed several rounds of user testing with internal and external merchants</li>
-        <li>Gathered insights on key workflows and merchant statistics, enabling us to track store and account switching and critical user tasks</li>
-        <li>Enabling up to 10 stores is done automatically on log-in, keeping performance high, covering 99.99% of merchants, and providing intuitive fallbacks</li>
-        <li>Released on-time to positive reviews and customer feedback</li>
-      </ul>
+        <h3>Solution</h3>
+        <p>
+          Our solution involved creating a new idiom in the app of surfaces. The main surface would include all of the features of your store that users were accustomed to: their home tab, analytics, products etc. Accounts and stores were moved to a new side-drawer, creating an elevated surface for contextual information. We also removed the 'Store' tab, and replaced it with a sheet where you can find additional features neatly organized. We styled this bottom sheet to act as an extension of the tab bar, and engineered it to allow for easy multitasking by maintining a unique back-stack for each tab.
+        </p>
+        <ul>
+          <li>Completed several rounds of user testing with internal and external merchants</li>
+          <li>Gathered insights on key workflows and merchant statistics, enabling us to track store and account switching and critical user tasks</li>
+          <li>Enabling up to 10 stores is done automatically on log-in, keeping performance high, covering 99.99% of merchants, and providing intuitive fallbacks</li>
+          <li>Released on-time to positive reviews and customer feedback</li>
+        </ul>
 
-      <div className='video-wrapper'>
-          <VideoPlayer url={video01} />
-          <VideoPlayer url={video02} />
-      </div>
-      <div className='image-caption'>Switching between stores and accounts in the side drawer and multi-tasking across features with the bottom sheet.</div>
+        <div className='video-wrapper'>
+            <VideoPlayer url={video01} />
+            <VideoPlayer url={video02} />
+        </div>
+        <div className='image-caption'>Switching between stores and accounts in the side drawer and multi-tasking across features with the bottom sheet.</div>
+      </section>
     </article>
   );
 };
